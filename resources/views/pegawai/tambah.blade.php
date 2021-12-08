24db5ae371de070b6c656adcfd7fd7dc5fd362f3
kwang0149 <hongkwang321@gmail.com>
2021-12-08 15:26:31 +0700

    Update tambah.blade.php

--- resources/views/pegawai/tambah.blade.php
+++ resources/views/pegawai/tambah.blade.php
@@ -12,10 +12,35 @@
  
 	<form action="/pegawai/store" method="post">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama" required="required"> <br/>
+		{{-- Nama <input type="text" name="nama" required="required"> <br/>
 		Jabatan <input type="text" name="jabatan" required="required"> <br/>
 		Umur <input type="number" name="umur" required="required"> <br/>
 		Alamat <textarea name="alamat" required="required"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+		<input type="submit" value="Simpan Data"> --}}
+
+			<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nama</label>
+    <input type="text" name="nama" required="required" class="form-control">
+    <div id="emailHelp" class="form-text">Masukkan Nama</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Jabatan</label>
+    <input type="text" name="jabatan" required="required" class="form-control">
+	<div id="emailHelp" class="form-text">Masukkan Jabatan</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Umur</label>
+    <input type="text" name="umur" required="required" class="form-control">
+    <div id="emailHelp" class="form-text">Masukkan Umur</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Alamat</label>
+    <input type="text" name="alamat" required="required" class="form-control">
+    <div id="emailHelp" class="form-text">Masukkan Alamat</div>
+  </div>
+  <div class="d-grid gap-2 col-6 mx-auto">
+                 <button class="btn btn-primary" type="submit">Simpan Data</button>
+        </div>
+
 	</form>
 	@endsection
